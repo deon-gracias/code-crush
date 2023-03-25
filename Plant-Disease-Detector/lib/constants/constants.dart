a25ccdc7dedfee56c09485cbd1ef4f32ed00cff8
omjadhav3714 <omjadhav963@gmail.com>
2023-03-25 23:27:37 +0530

    remove unwanted code

--- Plant-Disease-Detector/lib/constants/constants.dart
+++
@@ -1,6 +0,0 @@
-import 'package:flutter/material.dart';
-
-const kMain = Color(0xff00ad59);
-const kAccent = Color(0xff1ca800);
-const kSecondary = Color(0xff626463);
-const kWhite = Color(0xffffffff);
